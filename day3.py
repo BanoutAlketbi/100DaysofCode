0968350be11ec6ed38ad34afb0cd72ad884bd7e1
Banoot <banoot@Banoots-MBP-5.home>
2019-08-22 15:47:10 +0400

    day3

---
+++ day3.py
@@ -0,0 +1,37 @@
+x = 8
+y = "Adam"
+print (x)
+print (y)
+r = 5 # r is of type int
+r = "Sally" # r is now of type str
+print (r)
+
+x = "john"
+print (x)
+x = 'john'
+print (x)
+a, b, c = "Apple", "Orange", "Cherry"
+print (a)
+print (b)
+print (c)
+
+a = b = c = "Apple"
+print (a)
+print (b)
+print (c)
+
+t = "awesome"
+print ("Python is " + t)
+o = "python is "
+u = "grest"
+f = o + u 
+print (f)
+
+g = 2
+i = 11
+print (g + i)
+
+l = 3
+m = 'error'
+print ( l + m)
+
