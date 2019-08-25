2b90f38081d8bfad80105cfa78ef7b587bb5a643
Banoot <banoot@Banoots-MBP-5.home>
2019-08-25 19:35:34 +0400

    day6 file

---
+++ day6.py
@@ -0,0 +1,25 @@
+#---int() to convert to int
+q = int(2) # q will be 2
+w = int(34.3) # w will be 34 (without 3)
+e = int("1") # e will be 1
+print(q)
+print(w)
+print(e)
+
+#---float() to convert to float
+a = float(45) #a s z f will be float 
+s = float(12.23)
+z = float("1")
+f = float("23.4")
+print(a)
+print(s)
+print(z)
+print(f)
+
+#---str() to string
+y = str("up") # y r u will be string
+r = str(23)
+u = str(9.0)
+print(y)
+print(r)
+print(u)
