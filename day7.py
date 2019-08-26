78da5737a87d6a5c5a3e57ee3c4a295da4b17b53
Banoot <banoot@Banoots-MBP-5.home>
2019-08-26 19:13:18 +0400

    add day7

---
+++ day7.py
@@ -0,0 +1,26 @@
+#--- for a string we use either a ' or ""
+
+print("MyDubai")
+print('MyDubai')
+
+#--- to assign a variable we use = then what wewant to assign 
+
+x = 'Mac'
+print(x)
+
+#--- for multiline strings we use """ or '''
+
+y = '''A positive attitude causes a chain reaction of positive thoughts,
+events and outcomes. 
+It is a catalyst and it sparks extraordinary results.'''
+print(y)
+
+
+#--postions in python start from 0, tp get to single character we use []
+
+z = "Welcom to Python"
+print(z[2])
+
+#--- to get a character: for example py from z we use [] with the num of postion
+print(z[9:12])
+
