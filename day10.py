57a072a4c6faea39bff0ae175236a9fcc8331350
Banoot <banoot@Banoots-MBP-5.home>
2019-08-29 21:39:01 +0400

    day10

---
+++ day10.py
@@ -0,0 +1,20 @@
+#--- Arithmetic Operators
+x = 7
+y = 3
+
+print(x * y)
+print(x/y)
+print(x-y)
+print(y%x)
+
+a = 4
+a /=2
+print(a)
+v = 6
+v ^=3
+
+z = 10
+b = 12
+print(z>b)
+print(z<=b)
+print(b != z)
