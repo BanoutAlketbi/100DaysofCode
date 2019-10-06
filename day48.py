63222674a1d291a9cd637ece2c1865be69d32288
Banoot <banoot@Banoots-MBP-5.home>
2019-10-06 22:55:17 +0400

    day48

---
+++ day48.py
@@ -0,0 +1,12 @@
+def myfunction():
+    x = 23
+    def myinnerfunction():
+        print(x)
+    myinnerfunction()
+myfunction()
+#----------
+y = "python"
+def func1():
+    print(y)
+func1()
+print(y)
