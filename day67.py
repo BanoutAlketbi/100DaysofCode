fc78424d2608e1a808724a35474f2a9f376a4e6d
Banoot <banoot@Banoots-MBP-5.home>
2019-10-27 23:45:27 +0400

    files

---
+++ day67.py
@@ -0,0 +1,4 @@
+fletter = input("Kindly enter the first letter of your name here: ")
+lletter = input("Kindly enter the last letter of your name here: ")
+print("The first letter of your name is {}".format(fletter),
+"and the last letter is {}".format(lletter))
---
+++ day68.py
@@ -0,0 +1,5 @@
+cbalance = "Dear {}, Your current balance is {}{}"
+name = "Ahmad Ali"
+balance = 53.44
+currency = "$"
+print(cbalance.format(name, balance, currency))
